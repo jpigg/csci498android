e8d0016800b43253a2264ade6cae3b088e08861d
Jon Pigg <jpigg@mymail.mines.edu>
2012-09-02 19:22:55 -0600

    changed the activity_lunch_list.xml file to the one provided at the beginning of chapter 3 in apt

---
+++ LunchList/src/csci498/jpigg/lunchlist/Restaurant.java
@@ -0,0 +1,28 @@
+package csci498.jpigg.lunchlist;
+
+public class Restaurant {
+	
+	private String name = "";
+	private String address = "";
+	
+	public String getName()
+	{
+		return(name);
+	}
+	
+	public String getAddress()
+	{
+		return(address);
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+
+}
