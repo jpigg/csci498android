10aaa759feaa59409bd58edf9e19186f09128e7d
Jon Pigg <jpigg@mymail.mines.edu>
2012-09-13 23:11:46 -0600

    Added Initial list and details tabs

--- LunchList/src/csci498/jpigg/lunchlist/LunchList.java
+++ LunchList/src/csci498/jpigg/lunchlist/LunchList.java
@@ -1,4 +1,3 @@
-/*
 package csci498.jpigg.lunchlist;
 
 import java.util.*;
@@ -6,141 +5,13 @@ import java.util.*;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.LayoutInflater;
-import android.view.Menu;
+import android.app.TabActivity;
+import android.widget.TabHost;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
 
-public class LunchList extends Activity {
-	List<Restaurant> model = new ArrayList<Restaurant>();
-	ArrayAdapter<Restaurant> adapter = null;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lunch_list);
-        
-        Button save = (Button)findViewById(R.id.save);
-        
-        save.setOnClickListener(onSave);
-        
-        ListView list = (ListView)findViewById(R.id.restaurants);
-        adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, model);
-        list.setAdapter(adapter);
-    }
-
-    private View.OnClickListener onSave = new View.OnClickListener() {
-		
-		public void onClick(View v) {
-			Restaurant r = new Restaurant();
-			
-			EditText name = (EditText)findViewById(R.id.name);
-			EditText address = (EditText)findViewById(R.id.addr);
-			
-			r.setName(name.getText().toString());
-			r.setAddress(address.getText().toString());
-			
-			RadioGroup types = (RadioGroup)findViewById(R.id.types);
-			
-			switch (types.getCheckedRadioButtonId())
-			{
-				case R.id.sit_down:
-					r.setType("sit_down");
-					break;
-					
-				case R.id.take_out:
-					r.setType("take_out");
-					break;
-					
-				case R.id.delivery:
-					r.setType("delivery");
-					break;
-			}
-			
-			adapter.add(r);
-			
-		}
-	};
-	
-	class RestaurantAdapter extends ArrayAdapter<Restaurant> {
-		RestaurantAdapter()
-		{
-			super(LunchList.this, android.R.layout.simple_list_item_1, model);
-		}
-		
-		public View getView(int position, View convertView, ViewGroup parent)
-		{
-			View row = convertView;
-			RestaurantHolder holder = null;
-			
-			if(row == null)
-			{
-				LayoutInflater inflater = getLayoutInflater();
-				row = inflater.inflate(R.layout.row, parent, false);
-				holder = new RestaurantHolder(row);
-				row.setTag(holder);
-			}
-			else
-			{
-				holder = (RestaurantHolder)row.getTag();
-			}
-			
-			holder.populateFrom(model.get(position));
-			
-			return(row);
-		}
-		
-	}
-	
-	static class RestaurantHolder
-	{
-		private TextView name = null;
-		private TextView address = null;
-		private ImageView icon = null;
-		
-		RestaurantHolder(View row)
-		{
-			name = (TextView)row.findViewById(R.id.title);
-			address = (TextView)row.findViewById(R.id.address);
-			icon = (ImageView)row.findViewById(R.id.icon);
-		}
-		
-		void populateFrom(Restaurant r)
-		{
-			name.setText(r.getName());
-			address.setText(r.getAddress());
-			
-			if (r.getType().equals("sit_down"))
-			{
-				icon.setImageResource(R.drawable.ball_red);
-			}
-			else if (r.getType().equals("take_out"))
-			{
-				icon.setImageResource(R.drawable.ball_yellow);
-			}
-			else
-			{
-				icon.setImageResource(R.drawable.ball_green);
-			}
-		}
-	}
-	
-}
-*/
-
-package csci498.jpigg.lunchlist;
-
-import java.util.*;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.*;
-
-public class LunchList extends Activity {
+public class LunchList extends TabActivity {
 	List<Restaurant> model = new ArrayList<Restaurant>();
 	RestaurantAdapter adapter = null;
 
@@ -156,6 +27,21 @@ public class LunchList extends Activity {
         ListView list = (ListView)findViewById(R.id.restaurants);
         adapter = new RestaurantAdapter();
         list.setAdapter(adapter);
+        
+        //Tabs added in APT6
+        TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
+        
+        spec.setContent(R.id.restaurants);
+        spec.setIndicator("List", getResources().getDrawable(R.drawable.list));
+        
+        getTabHost().addTab(spec);
+        
+        spec = getTabHost().newTabSpec("tag2");
+        spec.setContent(R.id.details);
+        spec.setIndicator("Details", getResources().getDrawable(R.drawable.restaurant));
+        
+        getTabHost().addTab(spec);
+        getTabHost().setCurrentTab(0);
     }
 
     private View.OnClickListener onSave = new View.OnClickListener() {
@@ -242,20 +128,20 @@ public class LunchList extends Activity {
 			if (r.getType().equals("sit_down"))
 			{
 				icon.setImageResource(R.drawable.ball_red);
-				name.setTextColor(0xffff0000);
-				address.setTextColor(0xddff0000);
+				//name.setTextColor(0xffff0000);
+				//address.setTextColor(0xddff0000);
 			}
 			else if (r.getType().equals("take_out"))
 			{
 				icon.setImageResource(R.drawable.ball_yellow);
-				name.setTextColor(0xffffff00);
-				address.setTextColor(0xddffff00);
+				//name.setTextColor(0xffffff00);
+				//address.setTextColor(0xddffff00);
 			}
 			else
 			{
 				icon.setImageResource(R.drawable.ball_green);
-				name.setTextColor(0xff00ff00);
-				address.setTextColor(0xdd00ff00);
+				//name.setTextColor(0xff00ff00);
+				//address.setTextColor(0xdd00ff00);
 			}
 		}
 	}
