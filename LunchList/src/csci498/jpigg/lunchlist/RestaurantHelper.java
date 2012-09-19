c9f8c59da0a715830f1196555b1be54a451c4dce
Jon Pigg <jpigg@mymail.mines.edu>
2012-09-18 18:56:02 -0600

    Created RestarauntHelper.java and overrode onCreate and onUpdate

---
+++ LunchList/src/csci498/jpigg/lunchlist/RestaurantHelper.java
@@ -0,0 +1,33 @@
+package csci498.jpigg.lunchlist;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+
+public class RestaurantHelper extends SQLiteOpenHelper
+{
+	
+	private static final String DATABASE_NAME = "lunchlist.db";
+	private static final int SCHEMA_VERSION = 1;
+	
+	public RestaurantHelper(Context context)
+	{
+		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT);");
+	}
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		//nothing to do until another schema version is created
+	}
+
+}
