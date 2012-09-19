867643eb1d0c0b6812cd2fde28c72f3fcf680e8a
Jon Pigg <jpigg@mymail.mines.edu>
2012-09-18 19:03:55 -0600

    Removed extraneous code from LunchList.java

--- LunchList/src/csci498/jpigg/lunchlist/LunchList.java
+++ LunchList/src/csci498/jpigg/lunchlist/LunchList.java
@@ -27,14 +27,10 @@ public class LunchList extends TabActivity {
     EditText notes = null;
     RadioGroup types = null;
     Restaurant current = null;
-    AtomicBoolean isActive = new AtomicBoolean(true);
-    int progress;
-    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_lunch_list);
         
         //APT6 step 4
@@ -71,98 +67,27 @@ public class LunchList extends TabActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-    	new MenuInflater(this).inflate(R.menu.option, menu);
-    	
-    	return(super.onCreateOptionsMenu(menu));
+    	return true;
     }
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-    	if (item.getItemId() == R.id.toast)
-    	{
-    		String message = "No restaurant selected";
-    		
-    		if (current != null)
-    		{
-    			message = current.getNotes();
-    		}
-    		
-    		Toast.makeText(this,  message,  Toast.LENGTH_LONG).show();
-    		
-    		return(true);
-    	}
-    	else if (item.getItemId()==R.id.run)
-    	{
-    		startWork();
-    		
-    		return true;
-    	}
-    	
-    	return(super.onOptionsItemSelected(item));
+    	return true;
     }
     
     @Override
     public void onPause()
     {
-    	super.onPause();
-    	
-    	isActive.set(false);
+
     }
     
     @Override
     public void onResume()
     {
-    	super.onResume();
     	
-    	isActive.set(true);
-    	
-    	if (progress > 0)
-    	{
-    		startWork();
-    	}
     }
-    
-    public void startWork()
-    {
-    	setProgressBarVisibility(true);
-    	new Thread(longTask).start();
-    }
-    
-    private Runnable longTask = new Runnable()
-    {
-    	public void run()
-    	{
-    		for (int i = progress; i < 10000 && isActive.get(); i+=200)
-    		{
-    			doSomeLongWork(200);
-    		}
-    		
-    		if(isActive.get())
-    		{
-    			runOnUiThread(new Runnable(){
-    				public void run()
-    				{
-    					setProgressBarVisibility(false);
-    					progress = 0;
-    				}
-    			});
-    		}
-    	}
-    };
-    
-    private void doSomeLongWork(final int incr)
-    {
-    	runOnUiThread(new Runnable()
-    	{
-    		public void run()
-    		{
-    			progress += incr;
-    			setProgress(progress);
-    		}
-    	});
-    	SystemClock.sleep(250);
-    }
+
     
     private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
     	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
