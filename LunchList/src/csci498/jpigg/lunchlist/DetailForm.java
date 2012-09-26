74fb8199aa741e2ecefdf5d5e8b887cc5723df35
Jon Pigg <jpigg@mymail.mines.edu>
2012-09-25 18:59:41 -0600

    Adding detail_form.xml for the DetailForm activity

---
+++ LunchList/src/csci498/jpigg/lunchlist/DetailForm.java
@@ -0,0 +1,11 @@
+package csci498.jpigg.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
