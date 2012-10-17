a3650ed8da1a4290f262dfb258b59f51ae882a03
Jon Pigg <jpigg@mymail.mines.edu>
2012-10-17 00:49:15 -0600

    Updating FeedService to send the feed to the activity

---
+++ LunchList/src/csci498/jpigg/lunchlist/FeedService.java
@@ -0,0 +1,46 @@
+package csci498.jpigg.lunchlist;
+
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+public class FeedService extends IntentService{
+	
+	public static final String EXTRA_URL = "csci498.jpigg.lunchlish.EXTRA_URL";
+	public static final String EXTRA_MESSENGER = "csci498.jpigg.lunchlist.EXTRA_MESSENGER";
+	
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent i) {
+		RSSReader reader = new RSSReader();
+		Messenger messenger = (Messenger) i.getExtras().get(EXTRA_MESSENGER);
+		Message msg = Message.obtain();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+			
+			msg.arg1 = Activity.RESULT_OK;
+			msg.obj = result;
+		} catch (Exception e) {
+			Log.e("Lunchlist", "Exception parsing feed", e);
+			msg.arg1 = Activity.RESULT_CANCELED;
+			msg.obj = e;
+		}
+		
+		try {
+			messenger.send(msg);
+		} catch (Exception e) {
+			Log.w("LunchList", "Exception sending results to activity", e);
+		}
+	}
+
+}
