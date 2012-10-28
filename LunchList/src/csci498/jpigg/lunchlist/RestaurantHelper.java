22cb604f834f434e212ddcfa428b2f5a4c255171
Jon Pigg <jpigg@mymail.mines.edu>
2012-10-28 13:44:00 -0600

    Modifying RestaurantHelper to add support for location in the data model

--- LunchList/src/csci498/jpigg/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/jpigg/lunchlist/RestaurantHelper.java
@@ -12,7 +12,7 @@ public class RestaurantHelper extends SQLiteOpenHelper
 {
 	
 	private static final String DATABASE_NAME = "lunchlist.db";
-	private static final int SCHEMA_VERSION = 2;
+	private static final int SCHEMA_VERSION = 3;
 	
 	public RestaurantHelper(Context context)
 	{
@@ -47,7 +47,7 @@ public class RestaurantHelper extends SQLiteOpenHelper
 	
 	public Cursor getAll(String orderBy)
 	{
-		return(getReadableDatabase().rawQuery("SELECT _id,  name, address, type, notes, feed FROM restaurants ORDER BY " + orderBy, null));
+		return(getReadableDatabase().rawQuery("SELECT _id,  name, address, type, notes, lat, lon FROM restaurants ORDER BY " + orderBy, null));
 	}
 	
 	public String getName(Cursor c)
@@ -74,24 +74,50 @@ public class RestaurantHelper extends SQLiteOpenHelper
 		return(c.getString(5));
 	}
 	
+	public double getLatitude(Cursor c) {
+		return(c.getDouble(6));
+	}
+	
+	public double getLongitude(Cursor c) {
+		return(c.getDouble(7));
+	}
+	
 	
 	public Cursor getById(String id) {
 		String[] args = {id};
 		
-		return(getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed FROM restaurants WHERE _ID=?", args));
+		return(getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants WHERE _ID=?", args));
 	}
 	
 	
 	@Override
 	public void onCreate(SQLiteDatabase db)
 	{
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT);");
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);");
 	}
 	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
 	{
-		db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		if (oldVersion < 2) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
+		}
+		
+		if (oldVersion < 3) {
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lat REAL");
+			db.execSQL("ALTER TABLE restaurants ADD COLUMN lat REAL");
+		}
+		
+	}
+	
+	public void updateLocation(String id, double lat, double lon) {
+		ContentValues cv = new ContentValues();
+		String[] args = {id};
+		
+		cv.put("lat", lat);
+		cv.put("lon", lon);
+		
+		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
 	}
 
 }
