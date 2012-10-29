1c70cff4993b77211a9938d73b9d083af9199d33
Jon Pigg <jpigg@mymail.mines.edu>
2012-10-28 20:16:03 -0600

    Creating an itemized overlay for the map activity

--- LunchList/src/csci498/jpigg/lunchlist/DetailForm.java
+++ LunchList/src/csci498/jpigg/lunchlist/DetailForm.java
@@ -29,6 +29,8 @@ public class DetailForm extends Activity {
 	String restaurantId = null;
 	TextView location = null;
 	LocationManager locMgr = null;
+	double latitude = 0.0d;
+	double longitude = 0.0d;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -131,6 +133,10 @@ public class DetailForm extends Activity {
 		else if (item.getItemId()==R.id.map) {
 			Intent i = new Intent(this, RestaurantMap.class);
 			
+			i.putExtra(RestaurantMap.EXTRA_LATITUDE, latitude);
+			i.putExtra(RestaurantMap.EXTRA_LONGITUDE, longitude);
+			i.putExtra(RestaurantMap.EXTRA_NAME, name.getText().toString());
+			
 			startActivity(i);
 		}
 		
@@ -186,6 +192,9 @@ public class DetailForm extends Activity {
 		else {
 			types.check(R.id.delivery);
 		}
+		
+		latitude = helper.getLatitude(c);
+		longitude = helper.getLongitude(c);
 		
 		location.setText(String.valueOf(helper.getLatitude(c)) + ", " + String.valueOf(helper.getLongitude(c)));
 		
