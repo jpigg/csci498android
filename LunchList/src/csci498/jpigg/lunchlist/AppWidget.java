270c1e423ade96dc698d1ce8a971facda5d3c3cd
Jon Pigg <jpigg@mymail.mines.edu>
2012-11-25 17:12:46 -0700

    Adding honeycomb or later xml files for the widget and editing AppWidget to determine which xml file to use

--- LunchList/src/csci498/jpigg/lunchlist/AppWidget.java
+++ LunchList/src/csci498/jpigg/lunchlist/AppWidget.java
@@ -4,11 +4,15 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class AppWidget extends AppWidgetProvider{
 	
 	@Override
 	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			onHCUpdate(ctxt, mgr, appWidgetIds);
+		}
 		ctxt.startService(new Intent(ctxt, WidgetService.class));
 	}
 
