e967015d02ab345fb1d5b0650fe1fd790e3ee743
Jon Pigg <jpigg@mymail.mines.edu>
2012-11-26 15:50:56 -0700

    Editing AlarmActivity to play the ringtone when the alarm activity is loaded

--- LunchList/src/csci498/jpigg/lunchlist/AlarmActivity.java
+++ LunchList/src/csci498/jpigg/lunchlist/AlarmActivity.java
@@ -1,14 +1,49 @@
 package csci498.jpigg.lunchlist;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
-public class AlarmActivity extends Activity {
+public class AlarmActivity extends Activity implements MediaPlayer.OnPreparedListener {
+	
+	MediaPlayer player = new MediaPlayer();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alarm);
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		String sound = prefs.getString("alarm_ringtone",  null);
+		
+		if (sound != null) {
+			player.setAudioStreamType(AudioManager.STREAM_ALARM);
+			
+			try {
+				player.setDataSource(sound);
+				player.setOnPreparedListener(this);
+				player.prepareAsync();
+			} catch (Exception e) {
+				Log.e("Lunchlist", "Exception in playing the ringtone", e);
+			}
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		if (player.isPlaying()) {
+			player.stop();
+		}
+		
+		super.onPause();
+	}
+	
+	public void onPrepared(MediaPlayer player) {
+		player.start();
 	}
 	
 }
